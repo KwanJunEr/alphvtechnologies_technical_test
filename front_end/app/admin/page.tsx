bd884b07ae17babcef79fd64bf786a55b3a3c8de
KwanJunEr <jonastheitguy@gmail.com>
2024-10-18 22:12:03 +0800

    complete intial setup for front-end

---
+++ front_end/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Component from './component'
+
+const AdminPage = () => {
+  return (
+    <div>
+      <Component/>
+    </div>
+  )
+}
+
+export default AdminPage
---
+++ front_end/app/signin/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Component from './component'
+
+const SignInPage = () => {
+  return (
+    <div>
+        <Component/>
+    </div>
+  )
+}
+
+export default SignInPage
